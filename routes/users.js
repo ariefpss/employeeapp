a3a93550e0872b6580a4fde0b2c602a6c420c46f
ariefpss <permanaarief8@gmail.com>
2021-09-30 21:35:37 +0900

    Make function for create new user to database

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var User = require('../models').users;
+var Makeid = require('../service/generateId');
+
+
+//TODO: Get page
+router.get('/', (req, res) => {
+  res.render('index', { title: 'App Employee' });
 });
+
+router.get('/adduser', (req, res) => {
+  res.render('user/adduser', {title: 'App Employee'});
+});
+
+
+//TODO: Activity CRUD users
+router.post('/adduser', async(req, res, next) => {
+  const generateid = new Makeid();
+
+  const _id = await generateid.makeId(req.body.birthdate);
+
+  const createuser = await User.build({
+    id: _id,
+    name: req.body.name,
+    email: req.body.email,
+    mobile: req.body.mobile,
+    birthdate: req.body.birthdate,
+    address: req.body.address
+  });
+
+  await createuser.save();
+
+
+});
+
+
 
 module.exports = router;
